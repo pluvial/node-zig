2e314badba59f342ac742707141160fff39d28e1
jpaquim <joaopaquim@gmail.com>
2022-02-12 22:04:57 +0000

    Separate exports index.js and bin scripts

--- index.js
+++ index.js
@@ -3,18 +3,9 @@
 import * as fs from 'fs/promises';
 import * as path from 'path';
 import * as url from 'url';
-import { $ } from 'zx';
+import { $, ProcessOutput } from 'zx';
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-
-// ignore the first 2 entries in argv:
-// argv[0] is the node executable, argv[1] is this script
-const args = process.argv.slice(2);
-
-// TODO: remove lines below, changed due to migrating from zx to this script as the top-level runner script
-// ignore the first 3 entries in argv:
-// argv[0] is the node executable, argv[1] is the zx entrypoint, argv[2] is this script
-// const args = process.argv.slice(3);
 
 const arch = {
   x64: 'x86_64',
@@ -43,37 +34,24 @@ export async function install() {
   await $`curl -fsSL ${url} | tar xJ -C ${installDirectory} --strip-components=1`;
 }
 
-export async function run(...cliArgs) {
+export async function run(...args) {
   try {
     await fs.access(binaryPath);
   } catch (err) {
     throw new Error(`You must install ${name} before you can run it`);
   }
 
-  // if used as a function, pass any arguments to the zig CLI, otherwise use the
-  // arguments passed to the script itself, removing the first `mode` argument
-  const zigArgs = cliArgs.length > 0 ? cliArgs : args.slice(1);
-  const result = await $`${binaryPath} ${zigArgs}`;
-  process.exit(result.exitCode);
+  try {
+    await $`${binaryPath} ${args}`;
+  } catch (error) {
+    if (error instanceof ProcessOutput && error.exitCode) {
+      // console.error({ stderr: error.stderr, stdout: error.stdout });
+      process.exit(error.exitCode);
+    }
+    console.error(error);
+  }
 }
 
 export async function uninstall() {
   await $`rm -rf ${installDirectory}`;
-}
-
-const fns = { install, run, uninstall };
-const mode = args[0];
-const fn = fns[mode];
-
-if (!fn) {
-  const supported = Object.keys(fns);
-  console.error(`invalid script mode: ${args}\nSupported modes: ${supported}`);
-  process.exit(1);
-}
-
-try {
-  await fn();
-} catch (error) {
-  console.error(error);
-  process.exit(1);
 }
